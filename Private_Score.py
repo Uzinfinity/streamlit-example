0f0d449c5e14aa0287d75fb50ac17f992146f56d
Zijing Wu <wuzijing@seas.upenn.edu>
2022-06-20 16:45:33 -0400

    V1.1.2
    Model criteria change

--- Private_Score.py
+++ Private_Score.py
@@ -109,67 +109,87 @@ def calNDR(arrScale, NDR):
 
     if arrScale == '<$10M':
 
-        if NDR< 80:
+        if NDR < 80:
             return 0
 
-        if 80 < NDR <= 100:
-            return NDR / 100 * 25
-        elif 100 < NDR <= 115:
-            return 25 + (NDR - 100) / (115 - 100) * 25
-        elif 115 < NDR <= 130:
-            return 50 + (NDR - 115) / (130 - 115) * 25
+        if 80 <= NDR <= 93:
+            return (NDR - 80) / (93 - 80) * 25
+        elif 93 < NDR <= 105:
+            return 25 + (NDR - 93) / (105 - 93) * 25
+        elif 105 < NDR <= 117:
+            return 50 + (NDR - 105) / (117 - 105) * 25
+        elif 117 < NDR <=130:
+            return 75 + (NDR - 117) / (130 - 117) * 25
         else:
-            return 75 + (NDR - 130) / (150 - 130) * 25
+            return 100
 
     elif arrScale == '$10-$25M':
 
         if NDR < 80:
             return 0
-        
-        elif 80 < NDR <= 100:
-            return (NDR - 80) / (100 - 80) * 25
-        elif 100 < NDR <= 115:
-            return 25 + (NDR - 100) / (115 - 100) * 25
-        elif 115 < NDR <= 130:
-            return 50 + (NDR - 115) / (130 - 115) * 25
+
+        if 80 <= NDR <= 93:
+            return (NDR - 80) / (93 - 80) * 25
+        elif 93 < NDR <= 105:
+            return 25 + (NDR - 93) / (105 - 93) * 25
+        elif 105 < NDR <= 117:
+            return 50 + (NDR - 105) / (117 - 105) * 25
+        elif 117 < NDR <=130:
+            return 75 + (NDR - 117) / (130 - 117) * 25
         else:
-            return 75 + (NDR - 130) / (140 - 130) * 25
+            return 100
 
     elif arrScale == '$25-$50M':
 
-        if 80 < NDR <= 100:
-            return (NDR - 80) / (100 - 80) * 25
-        elif 100 < NDR <= 110:
-            return 25 + (NDR - 100) / (110 - 100) * 25
-        elif 110 < NDR <= 125:
-            return 50 + (NDR - 110) / (125 - 110) * 25
+        if NDR < 70:
+            return 0
+
+        if 70 <= NDR <= 80:
+            return (NDR - 70) / (80 - 70) * 25
+        elif 80 < NDR <= 90:
+            return 25 + (NDR - 80) / (90 - 80) * 25
+        elif 90 < NDR <= 110:
+            return 50 + (NDR - 90) / (110 - 90) * 25
+        elif 110 < NDR <= 130:
+            return 75 + (NDR - 110) / (130 - 110) * 25
         else:
-            return 75 + (NDR - 125) / (130 - 125) * 25
+            return 100
        
     elif arrScale == '$50-$100M':
 
-        if 80 < NDR <= 100:
-            return (NDR - 80) / (100 - 80) * 25
-        elif 100 < NDR <= 110:
-            return 25 + (NDR - 100) / (110 - 100) * 25
-        elif 110 < NDR <= 125:
-            return 50 + (NDR - 110) / (125 - 110) * 25
+        if NDR < 70:
+            return 0
+
+        if 70 <= NDR <= 80:
+            return (NDR - 70) / (80 - 70) * 25
+        elif 80 < NDR <= 90:
+            return 25 + (NDR - 80) / (90 - 80) * 25
+        elif 90 < NDR <= 110:
+            return 50 + (NDR - 90) / (110 - 90) * 25
+        elif 110 < NDR <= 130:
+            return 75 + (NDR - 110) / (130 - 110) * 25
         else:
-            return 75 + (NDR - 125) / (130 - 125) * 25
+            return 100
 
        
     elif arrScale == '$100M+':
 
-        if 80 < NDR <= 100:
-            return (NDR - 80) / (100 - 80) * 25
-        elif 100 < NDR <= 110:
-            return 25 + (NDR - 100) / (110 - 100) * 25
-        elif 110 < NDR <= 125:
-            return 50 + (NDR - 110) / (125 - 110) * 25
+        if NDR < 70:
+            return 0
+
+        if 70 <= NDR <= 80:
+            return (NDR - 70) / (80 - 70) * 25
+        elif 80 < NDR <= 90:
+            return 25 + (NDR - 80) / (90 - 80) * 25
+        elif 90 < NDR <= 110:
+            return 50 + (NDR - 90) / (110 - 90) * 25
+        elif 110 < NDR <= 130:
+            return 75 + (NDR - 110) / (130 - 110) * 25
         else:
-            return 75 + (NDR - 125) / (130 - 125) * 25
+            return 100
        
     return 0
+
 
 def calR40(arrScale, R40):
 
@@ -177,15 +197,20 @@ def calR40(arrScale, R40):
         return 0
 
     elif arrScale == '$25-$50M':
+        
+        if R40 < 10:
+            return 0
 
-        if R40 <= 40:
-            return R40 / 40 * 25
+        if 10 <= R40 <= 20:
+            return (R40 - 10) / (20 - 10) * 25
+        elif 20 < R40 <= 30:
+            return 25 + (R40 - 20) / (30 - 20) * 25
+        elif 30 < R40 <= 40:
+            return 50 + (R40 - 30) / (40 - 30) * 25
         elif 40 < R40 <= 60:
-            return 25 + (R40 - 40) / (60 - 40) * 25
-        elif 60 < R40 <= 105:
-            return 50 + (R40 - 60) / (105 - 60) * 25
+            return 75 + (R40 - 40) / (60 - 40) * 25
         else:
-            return 75 + (R40 - 105) / (120 - 105) * 25
+            return 100
        
     elif arrScale == '$50-$100M':
 
@@ -216,62 +241,84 @@ def calNMN(arrScale, NMN):
 
     if arrScale == '<$10M':
 
-        if not NMN:
+        if NMN < 0.2:
             return 0
     
-        if 0.2 < NMN <= 0.5:
+        if 0.2 <= NMN <= 0.5:
             return (NMN - 0.2) / (0.5 - 0.2) * 25
         elif 0.5 < NMN <= 0.8:
             return 25 + (NMN - 0.5) / (0.8 - 0.5) * 25
         elif 0.8 < NMN <= 1.4:
             return 50 + (NMN - 0.8) / (1.4 - 0.8) * 25
-        else:
+        elif 1 < NMN <= 1.3:
             return 75 + (NMN - 1.4) / (2.4 - 1.4) * 25
+        else:
+            return 100
     
     elif arrScale == '$10-$25M':
 
-        if 0.5 < NMN <= 0.8:
-            return (NMN - 0.5) / (0.8 - 0.5) * 25
-        elif 0.8 < NMN <= 1.0:
-            return 25 + (NMN - 0.8) / (1.0 - 0.8) * 25
-        elif 1.0 < NMN <= 1.4:
-            return 50 + (NMN - 1.0) / (1.4 - 1.0) * 25
+        if NMN < 0.2:
+            return 0
+    
+        if 0.2 <= NMN <= 0.5:
+            return (NMN - 0.2) / (0.5 - 0.2) * 25
+        elif 0.5 < NMN <= 0.8:
+            return 25 + (NMN - 0.5) / (0.8 - 0.5) * 25
+        elif 0.8 < NMN <= 1.4:
+            return 50 + (NMN - 0.8) / (1.4 - 0.8) * 25
+        elif 1 < NMN <= 1.3:
+            return 75 + (NMN - 1.4) / (2.4 - 1.4) * 25
         else:
-            return 75 + (NMN - 1.4) / (2.0 - 1.4) * 25
+            return 100
+
 
     elif arrScale == '$25-$50M':
 
-        if 0.6 < NMN <= 0.8:
-            return (NMN - 0.6) / (0.8 - 0.6) * 25
-        elif 0.8 < NMN <= 1.0:
-            return 25 + (NMN - 0.8) / (1.0 - 0.8) * 25
-        elif 1.0 < NMN <= 1.3:
-            return 50 + (NMN - 1.0) / (1.3 - 1.0) * 25
+        if NMN < 0.2:
+            return 0
+    
+        if 0.2 <= NMN <= 0.5:
+            return (NMN - 0.2) / (0.5 - 0.2) * 25
+        elif 0.5 < NMN <= 0.8:
+            return 25 + (NMN - 0.5) / (0.8 - 0.5) * 25
+        elif 0.8 < NMN <= 1.4:
+            return 50 + (NMN - 0.8) / (1.4 - 0.8) * 25
+        elif 1 < NMN <= 1.3:
+            return 75 + (NMN - 1.4) / (2.4 - 1.4) * 25
         else:
-            return 75 + (NMN - 1.3) / (1.5 - 1.3) * 25
+            return 100
        
     elif arrScale == '$50-$100M':
 
-        if 0.4 < NMN <= 0.6:
-            return (NMN - 0.4) / (0.6 - 0.4) * 25
-        elif 0.6 < NMN <= 0.8:
-            return 25 + (NMN - 0.6) / (0.8 - 0.6) * 25
-        elif 0.8 < NMN <= 1.1:
-            return 50 + (NMN - 0.8) / (1.1 - 0.8) * 25
+        if NMN < 0.2:
+            return 0
+    
+        if 0.2 <= NMN <= 0.5:
+            return (NMN - 0.2) / (0.5 - 0.2) * 25
+        elif 0.5 < NMN <= 0.8:
+            return 25 + (NMN - 0.5) / (0.8 - 0.5) * 25
+        elif 0.8 < NMN <= 1.4:
+            return 50 + (NMN - 0.8) / (1.4 - 0.8) * 25
+        elif 1 < NMN <= 1.3:
+            return 75 + (NMN - 1.4) / (2.4 - 1.4) * 25
         else:
-            return 75 + (NMN - 1.1) / (1.3 - 1.1) * 25
-
+            return 100
        
     elif arrScale == '$100M+':
 
-        if 0.3 < NMN <= 0.5:
-            return (NMN - 0.3) / (0.5 - 0.3) * 25
-        elif 0.5 < NMN <= 0.7:
-            return 25 + (NMN - 0.5) / (0.7 - 0.5) * 25
-        elif 0.7 < NMN <= 1.0:
-            return 50 + (NMN - 0.7) / (1.0 - 0.7) * 25
+        if NMN < 0.2:
+            return 0
+    
+        if 0.2 <= NMN <= 0.5:
+            return (NMN - 0.2) / (0.5 - 0.2) * 25
+        elif 0.5 < NMN <= 0.8:
+            return 25 + (NMN - 0.5) / (0.8 - 0.5) * 25
+        elif 0.8 < NMN <= 1.4:
+            return 50 + (NMN - 0.8) / (1.4 - 0.8) * 25
+        elif 1 < NMN <= 1.3:
+            return 75 + (NMN - 1.4) / (2.4 - 1.4) * 25
         else:
-            return 75 + (NMN - 1.0) / (1.2 - 1.0) * 25
+            return 100
        
     return 0
 
@@ -279,69 +326,87 @@ def calARRFTE(arrScale, ARRFTE):
 
     if arrScale == '<$10M':
 
-        if ARRFTE < 10000:
+        if ARRFTE < 30000:
             return 0
     
-        if 10000 < ARRFTE <= 30000:
-            return (ARRFTE - 10000) / (30000 - 10000) * 25
-        elif 30000 < ARRFTE <= 50000:
-            return 25 + (ARRFTE - 30000) / (50000 - 30000) * 25
-        elif 50000 < ARRFTE <= 73000:
-            return 50 + (ARRFTE - 50000) / (73000 - 50000) * 25
+        if 30000 <= ARRFTE <= 43000:
+            return (ARRFTE - 30000) / (43000 - 30000) * 25
+        elif 43000 < ARRFTE <= 55000:
+            return 25 + (ARRFTE - 43000) / (55000 - 43000) * 25
+        elif 55000 < ARRFTE <= 68000:
+            return 50 + (ARRFTE - 55000) / (68000 - 55000) * 25
+        elif 68000 < ARRFTE <= 80000:
+            return 75 + (ARRFTE - 68000) / (80000 - 68000) * 25
         else:
-            return 75 + (ARRFTE - 73000) / (105000 - 73000) * 25
+            return 100
     
     elif arrScale == '$10-$25M':
 
-        if ARRFTE < 60000:
+        if ARRFTE < 25000:
             return 0
-
-        if 60000 < ARRFTE <= 90000:
-            return (ARRFTE - 60000) / (90000 - 60000) * 25
-        elif 90000 < ARRFTE <= 120000:
-            return 25 + (ARRFTE - 90000) / (120000 - 90000) * 25
-        elif 120000 < ARRFTE <= 150000:
-            return 50 + (ARRFTE - 120000) / (150000 - 120000) * 25
+    
+        if 25000 <= ARRFTE <= 50000:
+            return (ARRFTE - 25000) / (50000 - 25000) * 25
+        elif 50000 < ARRFTE <= 75000:
+            return 25 + (ARRFTE - 50000) / (75000 - 50000) * 25
+        elif 75000 < ARRFTE <= 100000:
+            return 50 + (ARRFTE - 75000) / (100000 - 75000) * 25
+        elif 100000 < ARRFTE <= 125000:
+            return 75 + (ARRFTE - 100000) / (125000 - 100000) * 25
         else:
-            return 75 + (ARRFTE - 150000) / (170000 - 150000) * 25
+            return 100
 
     elif arrScale == '$25-$50M':
 
-        if 60000 < ARRFTE <= 80000:
-            return (ARRFTE - 60000) / (80000 - 60000) * 25
-        elif 80000 < ARRFTE <= 120000:
-            return 25 + (ARRFTE - 80000) / (120000 - 80000) * 25
-        elif 120000 < ARRFTE <= 150000:
-            return 50 + (ARRFTE - 120000) / (150000 - 120000) * 25
+        if ARRFTE < 50000:
+            return 0
+    
+        if 50000 <= ARRFTE <= 75000:
+            return (ARRFTE - 50000) / (75000 - 50000) * 25
+        elif 75000 < ARRFTE <= 100000:
+            return 25 + (ARRFTE - 75000) / (100000 - 75000) * 25
+        elif 100000 < ARRFTE <= 125000:
+            return 50 + (ARRFTE - 100000) / (125000 - 100000) * 25
+        elif 125000 < ARRFTE <= 150000:
+            return 75 + (ARRFTE - 125000) / (150000 - 125000) * 25
         else:
-            return 75 + (ARRFTE - 150000) / (195000 - 150000) * 25
-
+            return 100
 
        
     elif arrScale == '$50-$100M':
 
-        if 80000 < ARRFTE <= 120000:
-            return (ARRFTE - 80000) / (120000 - 80000) * 25
-        elif 120000 < ARRFTE <= 150000:
-            return 25 + (ARRFTE - 120000) / (150000 - 120000) * 25
+        if ARRFTE < 750000:
+            return 0
+    
+        if 75000 <= ARRFTE <= 100000:
+            return (ARRFTE - 75000) / (75000 - 100000) * 25
+        elif 100000 < ARRFTE <= 125000:
+            return 25 + (ARRFTE - 100000) / (125000 - 100000) * 25
+        elif 125000 < ARRFTE <= 150000:
+            return 50 + (ARRFTE - 125000) / (150000 - 125000) * 25
         elif 150000 < ARRFTE <= 175000:
-            return 50 + (ARRFTE - 150000) / (175000 - 150000) * 25
+            return 75 + (ARRFTE - 150000) / (175000 - 150000) * 25
         else:
-            return 75 + (ARRFTE - 175000) / (230000 - 175000) * 25
+            return 100
 
 
 
        
     elif arrScale == '$100M+':
 
-        if 100000 < ARRFTE <= 120000:
-            return (ARRFTE - 100000) / (120000 - 100000) * 25
-        elif 120000 < ARRFTE <= 150000:
-            return 25 + (ARRFTE - 120000) / (150000 - 120000) * 25
-        elif 150000 < ARRFTE <= 175000:
-            return 50 + (ARRFTE - 150000) / (175000 - 150000) * 25
+        if ARRFTE < 75000:
+            return 0
+    
+        if 75000 <= ARRFTE <= 100000:
+            return (ARRFTE - 30000) / (43000 - 30000) * 25
+        elif 43000 < ARRFTE <= 55000:
+            return 25 + (ARRFTE - 43000) / (55000 - 43000) * 25
+        elif 55000 < ARRFTE <= 68000:
+            return 50 + (ARRFTE - 55000) / (68000 - 55000) * 25
+        elif 68000 < ARRFTE <= 80000:
+            return 75 + (ARRFTE - 68000) / (80000 - 68000) * 25
         else:
-            return 75 + (ARRFTE - 175000) / (230000 - 175000) * 25
+            return 100
        
     return 0
 
@@ -358,53 +423,74 @@ def calBurnRate(arrScale, burnRate):
             return 25 + (burnRate - 0.2) / (0.4 - 0.2) * 25
         elif 0.4 < burnRate <= 0.6:
             return 50 + (burnRate - 0.4) / (0.6 - 0.4) * 25
-        else:
+        elif 0.6 < burnRate <= 0.8:
             return 75 + (burnRate - 0.6) / (0.8 - 0.6) * 25
+        else:
+            return 100
 
     elif arrScale == '$10-$25M':
 
-        if  0.2 < burnRate <= 0.4:
+        if not burnRate:
+            return 0
+
+        if  burnRate <= 0.2:
+            return burnRate / 0.2 * 25
+        elif 0.2 < burnRate <= 0.4:
+            return 25 + (burnRate - 0.2) / (0.4 - 0.2) * 25
+        elif 0.4 < burnRate <= 0.6:
+            return 50 + (burnRate - 0.4) / (0.6 - 0.4) * 25
+        elif 0.6 < burnRate <= 0.8:
+            return 75 + (burnRate - 0.6) / (0.8 - 0.6) * 25
+        else:
+            return 100
+
+    elif arrScale == '$25-$50M':
+
+        if burnRate < 0.2:
+            return 0
+
+        if  0.2 <= burnRate <= 0.4:
             return (burnRate - 0.2) / (0.4 - 0.2) * 25
         elif 0.4 < burnRate <= 0.6:
             return 25 + (burnRate - 0.4) / (0.6 - 0.4) * 25
         elif 0.6 < burnRate <= 0.8:
             return 50 + (burnRate - 0.6) / (0.8 - 0.6) * 25
-        else:
+        elif 0.8 < burnRate <= 1.0:
             return 75 + (burnRate - 0.8) / (1.0 - 0.8) * 25
+        else:
+            return 100
 
-    elif arrScale == '$25-$50M':
+    elif arrScale == '$50-$100M':
 
-        if  0.4 < burnRate <= 0.6:
+        if burnRate < 0.4:
+            return 0
+
+        if  0.4 <= burnRate <= 0.6:
             return (burnRate - 0.4) / (0.6 - 0.4) * 25
         elif 0.6 < burnRate <= 0.8:
             return 25 + (burnRate - 0.6) / (0.8 - 0.6) * 25
         elif 0.8 < burnRate <= 1.0:
             return 50 + (burnRate - 0.8) / (1.0 - 0.8) * 25
-        else:
+        elif 1.0 < burnRate <= 1.2:
             return 75 + (burnRate - 1.0) / (1.2 - 1.0) * 25
+        else:
+            return 100
        
-    elif arrScale == '$50-$100M':
+    elif arrScale == '$100M+':
 
-        if  0.6 < burnRate <= 0.8:
+        if burnRate < 0.6:
+            return 0
+
+        if  0.6 <= burnRate <= 0.8:
             return (burnRate - 0.6) / (0.8 - 0.6) * 25
         elif 0.8 < burnRate <= 1.0:
             return 25 + (burnRate - 0.8) / (1.0 - 0.8) * 25
         elif 1.0 < burnRate <= 1.2:
             return 50 + (burnRate - 1.0) / (1.2 - 1.0) * 25
-        else:
-            return 75 + (burnRate - 1.2) / (1.4 - 1.2) * 25
-
-       
-    elif arrScale == '$100M+':
-
-        if 0.8 < burnRate <= 1.0:
-            return (burnRate - 0.8) / (1.0 - 0.8) * 25
-        elif 1.0 < burnRate <= 1.2:
-            return 25 + (burnRate - 1.0) / (1.2 - 1.0) * 25
         elif 1.2 < burnRate <= 1.4:
-            return 50 + (burnRate - 1.2) / (1.4 - 1.2) * 25
+            return 75 + (burnRate - 1.2) / (1.4 - 1.2) * 25
         else:
-            return 75 + (burnRate - 1.4) / (1.6 - 1.4) * 25
+            return 100
        
     return 0
 
@@ -413,32 +499,32 @@ st.metric(label = 'ARR scale', value = arrScale)
 # st.write('You selected:')
 
 arrGrowth = st.number_input('ARR Growth (in %)', step = 1)
-st.metric(label = 'ARR Growth Score', value = calArrGrowth(arrScale, arrGrowth))
+st.metric(label = 'ARR Growth Score', value = round(calArrGrowth(arrScale, arrGrowth),2))
 # st.text('ARR Growth Score is ' + str(calArrGrowth(arrScale, arrGrowth)))
-# st.write('ARR Growth Score is ', int(arrGrowth), '%')
+st.write('ARR Growth is ', int(arrGrowth), '%')
 
 NDR = st.number_input('NDR (in %)', step = 1)
-st.metric(label = 'Net Dollar Retention Score', value = calNDR(arrScale, NDR))
+st.metric(label = 'Net Dollar Retention Score', value = round(calNDR(arrScale, NDR),2))
 # st.text('Net Dollar Retention Score is ' + str(calNDR(arrScale, NDR)))
-# st.write('Net Dollar Retention Score is ', int(NDR), '%')
+st.write('Net Dollar Retention is ', int(NDR), '%')
 
-R40 = st.number_input('Rule of 40')
-st.metric(label = 'Rule of 40 Score' , value = calR40(arrScale, R40))
+R40 = st.number_input('Rule of 40 (in %)')
+st.metric(label = 'Rule of 40 Score' , value = round(calR40(arrScale, R40),2))
 # st.text('Rule of 40 Score is ' +  str(calR40(arrScale,R40)))
-# st.write('Rule of 40 Score is ', int(R40), '%')
+st.write('Rule of 40 is ', R40)
 
 NMN = st.number_input('Net Magic Number')
-st.metric(label = 'Net Magic Number Score', value = calNMN(arrScale, NMN))
+st.metric(label = 'Net Magic Number Score', value = round(calNMN(arrScale, NMN),2))
 # st.write('Net Magic Number Score is ', calNMN(arrScale,NMN))
-# st.write('Net Magic Number Score is ', NMN)
+st.write('Net Magic Number is ', NMN)
 
 ARRFTE = st.number_input('ARR per FTE (in k)') * 1000
-st.metric(label = 'ARR per FTE Score', value = calARRFTE(arrScale, ARRFTE))
-# st.write('ARR per FTE Score is ', int(ARRFTE),'k')
+st.metric(label = 'ARR per FTE Score', value = round(calARRFTE(arrScale, ARRFTE)))
+st.write('ARR per FTE is ', ARRFTE,'k')
 
-burnRate = st.number_input('Burn Rate', max_value=1.0, step = 0.1)
-st.metric(label = 'Burn Rate Score ', value = calBurnRate(arrScale, burnRate))
-# st.write('Burn Rate Score is ', burnRate)
+burnRate = st.number_input('Burn Rate', max_value=1.0)
+st.metric(label = 'Burn Rate Score ', value = round(calBurnRate(arrScale, burnRate),2))
+st.write('Burn Rate is ', round(burnRate, 2))
 
 score = int(calScore(arrScale, arrGrowth,NDR,R40,NMN,ARRFTE,burnRate))
 
